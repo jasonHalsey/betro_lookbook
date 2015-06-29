2a80791103ff3a5c2378ece8186cbb14dc1b4ba8
Jason Halsey <jasonhalsey@Halsey-C2QNF00HFTPR.local>
2015-06-29 14:54:54 -0700

    add slider menu and back-to-top

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 Look Book Landing Page - A BlvdBrands Site
 
 // -- jQuery Plugins
+
 LazyLoad:
 http://www.appelsiini.net/projects/lazyload
 
 Waypoints:
 http://imakewebthings.com/waypoints/
+
+
+// -- Fonts
+Icon Fonts
+https://icomoon.io
---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+  
+// Refills sliding menu
+  $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+    $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+
+// Lazyload images
+  $(function() {
+    $("img.lazy").lazyload();
+    console.log("loaded");
+  });
+
+//Back To Top Scrolling
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 300,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$back_to_top = $('.cd-top');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
+	});
+
+	//smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+
+
+}); //end doc.ready
