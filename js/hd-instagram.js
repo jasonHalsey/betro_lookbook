9a39ef58245ffbdc408241191b36c980ff4c8692
Jason Halsey <jasonhalsey@Halsey-C2QNF00HFTPR.local>
2015-07-07 09:02:50 -0700

    add instagram feed

--- js/hd-instagram.js
+++ js/hd-instagram.js
@@ -2,12 +2,12 @@ $( document ).ready(function() {
 
 
 	jQuery.fn.spectragram.accessData = {
-	    accessToken: '1517312917.78d68d2.4bafa9ebfcf74d7b8d46d3ab9c417515',
-	    clientID: '78d68d2abad5427787753951f5e11376'
+	    accessToken: '1693648712.300ab90.b919d8e91c344ab8a7bd58f299674c4c',
+	    clientID: '300ab90307264815880a6698e420f317'
 	};
 
 	$('ul.jhalzfeed').spectragram('getUserFeed',{
-	    query: 'onebxwd',
+	    query: 'betrosimonecollection',
 	    max: 32,
 	    size: 'big',
 	    wrapEachWith: '<li class="instagram_image"></li>'
